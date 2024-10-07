d25ce460c8ece2ecc58c9ce060be6dba23f027e3
Guillaume Baudart <guillaume.baudart@inria.fr>
2024-10-07 15:02:19 +0200

    Check benchmark before running

--- nlir-bench.py
+++ nlir-bench.py
@@ -1,13 +1,35 @@
 import os
+import sys
 import hydra
 import json
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig
-from pytanque import Pytanque
+from pytanque import Pytanque, PetanqueError
 from nlir.agent import GPT
 from nlir.petanque import TacticEnv, TemplateEnv
 from nlir.search import naive_search
 from pathlib import Path
+
+
+def check_config(pet: Pytanque, wk_path: Path, cfg: DictConfig):
+    n_files = 0
+    n_thms = 0
+    errors = []
+    for thms in cfg.benchmark:
+        file_path = Path(wk_path, thms.file).absolute()
+        n_files += 1
+        for thm in thms.theorems:
+            try:
+                pet.start(str(file_path), thm)
+                n_thms += 1
+            except PetanqueError as err:
+                errors.append(f"- File {thms.file} {thm}: {err.message}")
+    if not errors:
+        print(f"Benchmarking {n_thms} theorems in {n_files} files")
+    else:
+        print(f"Config contains the following errors:", file=sys.stderr)
+        print("\n".join(errors), file=sys.stderr)
+        sys.exit(1)
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
@@ -17,6 +39,8 @@ def run(cfg: DictConfig):
     pet = Pytanque(cfg.petanque.address, cfg.petanque.port)
     pet.connect()
     pet.set_workspace(False, str(wk_path))
+
+    check_config(pet, wk_path, cfg)
 
     log_dir = HydraConfig.get().runtime.output_dir
 
@@ -46,7 +70,7 @@ def run(cfg: DictConfig):
         print(f"From {thms.file}")
         for thm in thms.theorems:
             print(f"  Try to prove {thm}")
-            file_path = Path(wk_path, thms.file)
+            file_path = Path(wk_path, thms.file).absolute()
             env = env_cls(pet, str(wk_path), str(file_path), thm, cfg.petanque.context)
             log_file = os.path.join(log_dir, f"{thms.file}:{thm}.jsonl")
             agent = GPT(
